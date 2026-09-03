205ea38690d8779673dfc34acbc55bcd6bae6c94
sehaxe <sehaxe@proton.me>
2026-09-03 20:24:01 +0300

    Лабораторная работа 1: задачи 1-4, README с разбором, Makefile

---
+++ home/1/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+//Задача 1. Вариант 5
+
+int main(void) {
+    float a,b;
+
+    printf("Введите катеты прямоугольного треугольника (через проблел, enter не имеет значения\n");
+
+    if (scanf("%f %f", &a, &b) != 2 || a <= 0 || b <= 0){
+        printf("Ошибка ввода!\n");
+        return 1;
+    }
+
+    printf("Гипотенуза: %.2f\n", sqrt(a * a + b * b));
+
+    printf("Площадь: %.2f\n", a * b / 2);
+
+    return 0;
+}
---
+++ home/4/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+//Задача 4. Вариант 5
+
+int main(void){
+
+    int status;
+
+    printf("Введите время года\n");
+    printf("1.Зима\n");
+    printf("2.Весна\n");
+    printf("3.Лето\n");
+    printf("4.Осень\n");
+
+    if (scanf("%d", &status) != 1 || status <= 0 || status >= 5){
+        printf("ТЫ НЕ ПРОЙДЕШЬ!!!!\n");
+        return 1;
+    }
+
+    switch (status) {
+        case 1:
+            printf("Декабрь, Январь, Февраль\n");
+            break;
+        case 2:
+            printf("Март, Апрель, Май\n");
+            break;
+        case 3:
+            printf("Июнь, Июль, Август\n");
+            break;
+        case 4:
+            printf("Сентябрь, Октябрь, Ноябрь\n");
+            break;
+        default:
+            printf("Как ты сюда попал, но это очень впечитляет\n");
+    }
+
+    return 0;
+
+}
